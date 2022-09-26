7bb0c19a0fc76c34aaba8dac2f77bd77f82cc5f2
Camille Laborde <camille.laborde@act21.fr>
2022-09-26 22:00:02 +0200

    update general layout

--- components/ChatList/ChatList.jsx
+++ components/ChatList/ChatList.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ChatListItem from './ChatListItem';
 
 export default function ChatList(props) {
-    return props.items.map(user =>
+    const items = props.items.map(user =>
         <ChatListItem
             key={user.id}
             name={user.name}
             lastMessage={user.lastMessage}
             avatar={user.avatar}>
         </ChatListItem>);
+
+    return (<div>
+        {items}
+    </div>)
 }
--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,9 +1,22 @@
 import ChatList from '../../components/ChatList/ChatList';
 import fsPromises from 'fs/promises';
 import path from 'path'
+import ChatRoom from '../../components/ChatRoom/ChatRoom';
+import ChatDetails from '../../components/ChatDetails/ChatDetails';
 
 export default function ChatHome({ rooms }) {
-    return <ChatList items={rooms}></ChatList>
+    return <div className='flex'>
+        <div className='flex-none'>
+            <ChatList items={rooms}></ChatList>
+        </div>
+        <div className='flex-1'>
+            <ChatRoom ></ChatRoom>
+        </div>
+        <div className='flex-none'>
+            <ChatDetails></ChatDetails>
+        </div>
+    </div>
+
 }
 
 export async function getStaticProps() {
